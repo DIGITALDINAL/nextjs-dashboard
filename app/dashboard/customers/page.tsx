34f1cea48c61be252e9ab6c63e978e4a357219f3
DIGITALDINAL <ronal.aldinal21@gmail.com>
2025-05-17 17:13:34 +0700

    refactor: remove unused components and imports from customers and dashboard pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,6 @@
 import { fetchCustomersPages } from "@/app/lib/data";
-import { CreateCustomer } from "@/app/ui/customers/buttons";
 import Table from "@/app/ui/customers/table";
-import { lusitana } from "@/app/ui/fonts";
 import Pagination from "@/app/ui/invoices/pagination";
-import Search from "@/app/ui/search";
 import { CustomersTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
@@ -20,13 +17,6 @@ export default async function Page(props: {
 
   return (
     <div className="w-full">
-      {/* <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search customers..." />
-        <CreateCustomer />
-      </div> */}
       <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
